cca772c499d36254277d5677ce309dccccaec7ec
Sourabh70022 <sourabh70022@gmail.com>
2023-09-14 00:41:06 +0530

    new icon

--- src/CandidateCard.jsx
+++ src/CandidateCard.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import icons from './assets/icon.png';
+
 const CandidateCard = ({ candidate }) => (
   <div className="card ">
     <h2 className='fontnew text-2xl font-semibold'>{candidate.name}</h2>
     
     <div className="placeholder-icons">
-      <div className="placeholder-icon">
+      <div className="placeholder-icon flex items-center">
         <img src={icons} alt="Placeholder Icon 1" />
         <p className='pl-2 fontnew'>{candidate.last_updated_at}</p>
       </div>
-      <div className="placeholder-icon">
+      <div className="placeholder-icon flex items-center">
         <img src={icons} alt="Placeholder Icon 2" />
         <p className='pl-2 fontnew'>{candidate.location}</p>
       </div>
-      <div className="placeholder-icon">
+      <div className="placeholder-icon flex items-center">
         <img src={icons} alt="Placeholder Icon 3" />
         <p className='pl-2 fontnew'>{candidate.gender}</p>
       </div>
